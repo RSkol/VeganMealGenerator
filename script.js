5a7487e1e1927bb06746d6440d915da0f9235fd4
RSkol <amzronai@gmail.com>
2021-01-20 22:50:07 -0300

    create meal object and randomize function

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 // Let there be light!
+
+let meal = {
+    starch: ["brown rice","baked potatoes","quinoa", "mashed potatoes"],
+    fruit:["grapes","apple","peach"],
+    drink:["grape juice","orange juice", "water"],
+    legume:["black beans", "hummus", "split peas"],
+    green:["lettuce","collard greens", "kale", "arugula"],
+    veggie:["steamed carrots","brocolli","greek salad"],
+    soup:["pea soup", "pumpkin soup", "miso soup", "spinach soup"]
+};
+
+function randomize (arr) {
+    return arr[Math.floor(Math.random() * arr.length)];
+}
+
+console.log ("This is our suggested meal...\n");
+for (item in meal) {
+    console.log (randomize(meal[item]));
+}
